07c3ae1265a17fe102a24fdfcba22d0755234951
vladi <vladislav.yushkov@mail.ru>
2023-08-18 20:05:51 +0300

    Test1

---
+++ app/src/main/java/com/example/sportstheme/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.sportstheme
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
+import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sportstheme.R
+import com.google.firebase.FirebaseApp
+import com.google.firebase.remoteconfig.BuildConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+
+        remoteConfig = FirebaseRemoteConfig.getInstance()
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(3600)
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val webViewUrl = remoteConfig.getString("webview_url")
+                    if (webViewUrl.isNotEmpty()) {
+                        if (isNetworkAvailable()) {
+                            webView.loadUrl(webViewUrl)
+                        } else {
+                            webView.loadUrl("file:///android_asset/no_connection.html")
+                        }
+                    } else {
+                        webView.loadUrl("file:///android_asset/sport_placeholder.html")
+                    }
+                } else {
+                    webView.loadUrl("file:///android_asset/error.html")
+                }
+            }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnected == true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webView.restoreState(savedInstanceState)
+    }
+
+    private fun checkIsEmu(): Boolean {
+        if (BuildConfig.DEBUG) return false
+        val phoneModel = Build.MODEL
+        val buildProduct = Build.PRODUCT
+        val buildHardware = Build.HARDWARE
+        var result = (Build.FINGERPRINT.startsWith("generic")
+                || phoneModel.contains("google_sdk")
+                || phoneModel.lowercase(Locale.getDefault()).contains("droid4x")
+                || phoneModel.contains("Emulator")
+                || phoneModel.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || buildHardware == "goldfish"
+                || Build.BRAND.contains("google")
+                || buildHardware == "vbox86"
+                || buildProduct == "sdk"
+                || buildProduct == "google_sdk"
+                || buildProduct == "sdk_x86"
+                || buildProduct == "vbox86p"
+                || Build.BOARD.lowercase(Locale.getDefault()).contains("nox")
+                || Build.BOOTLOADER.lowercase(Locale.getDefault()).contains("nox")
+                || buildHardware.lowercase(Locale.getDefault()).contains("nox")
+                || buildProduct.lowercase(Locale.getDefault()).contains("nox"))
+        if (result) return true
+        result = result or (Build.BRAND.startsWith("generic") &&
+                Build.DEVICE.startsWith("generic"))
+        if (result) return true
+        result = result or ("google_sdk" == buildProduct)
+        return result
+    }
+}
